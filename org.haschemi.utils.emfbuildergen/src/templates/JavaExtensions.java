23209b00d4ea74f0fd02ad53b7b9774fd1e53e7c
Siamak Haschemi <siamak.h@gmx.de>
2011-05-30 22:17:22 +0200

    - added UML2 builder example project - adapted builder generation to new findings in the UML2 metamodel (pluralizedGetters feature in UML Genmodel)

--- org.haschemi.utils.emfbuildergen/src/templates/JavaExtensions.java
+++ org.haschemi.utils.emfbuildergen/src/templates/JavaExtensions.java
@@ -12,8 +12,52 @@
  *******************************************************************************/
 package templates;
 
+import org.eclipse.emf.ecore.EStructuralFeature;
+
 public class JavaExtensions {
-  public static String javaPackage(final String p_javaClass) {
-    return p_javaClass.substring(0, p_javaClass.lastIndexOf("."));
-  }
+	public static String javaPackage(final String p_javaClass) {
+		return p_javaClass.substring(0, p_javaClass.lastIndexOf("."));
+	}
+
+	public static void throwRuntimeException(final String p_message) {
+		throw new RuntimeException(p_message);
+	}
+
+	/**
+	 * Code taken from org.eclipse.uml2.codegen.ecore.Generator.
+	 */
+	public static String pluralize(String name) {
+		if (name.equalsIgnoreCase("children") || name.endsWith("Children")) { //$NON-NLS-1$ //$NON-NLS-2$
+			return name;
+		} else if (name.equalsIgnoreCase("child") || name.endsWith("Child")) { //$NON-NLS-1$ //$NON-NLS-2$
+			return name + "ren"; //$NON-NLS-1$
+		} else if (name.equalsIgnoreCase("data") || name.endsWith("Data")) { //$NON-NLS-1$ //$NON-NLS-2$
+			return name;
+		} else if (name.equalsIgnoreCase("datum") || name.endsWith("Datum")) { //$NON-NLS-1$ //$NON-NLS-2$
+			return name.substring(0, name.length() - 2) + "a"; //$NON-NLS-1$
+		} else if (name.endsWith("By")) { //$NON-NLS-1$
+			return name + "s"; //$NON-NLS-1$
+		} else if (name.endsWith("y")) { //$NON-NLS-1$
+			return name.substring(0, name.length() - 1) + "ies"; //$NON-NLS-1$
+		} else if (name.endsWith("ex")) { //$NON-NLS-1$
+			return name.substring(0, name.length() - 2) + "ices"; //$NON-NLS-1$
+		} else if (name.endsWith("x")) { //$NON-NLS-1$
+			return name + "es"; //$NON-NLS-1$
+		} else if (name.endsWith("us")) { //$NON-NLS-1$
+			return name.substring(0, name.length() - 2) + "i"; //$NON-NLS-1$
+		} else if (name.endsWith("ss")) { //$NON-NLS-1$
+			return name + "es"; //$NON-NLS-1$
+		} else if (name.endsWith("s")) { //$NON-NLS-1$
+			return name;
+		} else {
+			return name + "s"; //$NON-NLS-1$
+		}
+	}
+	
+	public String normalizedName(EStructuralFeature p_structuralFeature) {
+		if(p_structuralFeature.getName().equals("class")) {
+			return "class_";
+		}
+		return p_structuralFeature.getName();
+	}
 }
