e072c281cbc5d367e3bcfacbebe1265b811efcc8
Siamak Haschemi <siamak.h@gmx.de>
2011-06-02 12:18:18 +0200

    - relaxed version ranges

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ssh haschemi@metrikforge.informatik.hu-berlin.de mkdir -p /var/update-sites/EMFBuilderGenerator/releases
+ssh haschemi@metrikforge.informatik.hu-berlin.de rm -rf /var/update-sites/EMFBuilderGenerator/releases/*
+scp -r org.haschemi.utils.emfbuildergen.updatesite/target/site/* haschemi@metrikforge.informatik.hu-berlin.de:/var/update-sites/EMFBuilderGenerator/releases
+ssh haschemi@metrikforge.informatik.hu-berlin.de chmod -R 755 /var/update-sites/EMFBuilderGenerator
